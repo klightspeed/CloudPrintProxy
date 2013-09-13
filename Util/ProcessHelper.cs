3b1095a093054d14b7ae69cf1bf4df8e529912f5
Ben Peddell <klightspeed@killerwolves.net>
2013-09-14 00:21:24 +1000

    Moved process functions to separate class.

---
+++ Util/ProcessHelper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace TSVCEO.CloudPrint.Util
+{
+    public class ProcessHelper
+    {
+        public static string EscapeCommandLineArgument(string arg)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringReader rdr = new StringReader(arg);
+            int c;
+            sb.Append('"');
+
+            while ((c = rdr.Read()) > 0)
+            {
+                if (c == '"')
+                {
+                    sb.Append("\\\"");
+                }
+                else if (c == '\\')
+                {
+                    int nrbackslash = 1;
+
+                    while (rdr.Peek() == '\\')
+                    {
+                        nrbackslash++;
+                        rdr.Read();
+                    }
+
+                    if (rdr.Peek() == '"')
+                    {
+                        sb.Append(new String('\\', nrbackslash * 2));
+                    }
+                    else
+                    {
+                        sb.Append(new String('\\', nrbackslash));
+                    }
+                }
+                else
+                {
+                    sb.Append((char)c);
+                }
+            }
+
+            sb.Append('"');
+            return sb.ToString();
+        }
+
+        public static string CreateCommandArguments(string[] args)
+        {
+            return String.Join(" ", args.Select(s => EscapeCommandLineArgument(s)).ToArray());
+        }
+
+        private static Process CreateProcessAsUser(string username, string domain, SecureString password, string exename, string[] args)
+        {
+            Process proc = new Process();
+
+            ProcessStartInfo startinfo = new ProcessStartInfo
+            {
+                Arguments = CreateCommandArguments(args),
+                FileName = exename,
+                CreateNoWindow = true,
+                ErrorDialog = false,
+                LoadUserProfile = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                StandardOutputEncoding = Encoding.UTF8,
+                StandardErrorEncoding = Encoding.UTF8,
+                UseShellExecute = false,
+                WorkingDirectory = Environment.GetEnvironmentVariable("SYSTEMDRIVE") + "\\",
+                UserName = username,
+                Domain = domain,
+                Password = password
+            };
+
+            proc.StartInfo = startinfo;
+
+            return proc;
+        }
+
+        public static int RunProcessAsUser(string username, string domain, SecureString password, Stream stdin, Stream stdout, Stream stderr, string exename, string[] args)
+        {
+            return RunProcessAsUser(username, domain, password, new StreamReader(stdin, Encoding.UTF8, false), new StreamWriter(stdout, Encoding.UTF8), new StreamWriter(stderr, Encoding.UTF8), exename, args);
+        }
+
+        public static int RunProcessAsUser(string username, string domain, SecureString password, TextReader stdin, TextWriter stdout, TextWriter stderr, string exename, string[] args)
+        {
+            using (Process proc = CreateProcessAsUser(username, domain, password, exename, args))
+            {
+                proc.Start();
+                Task stdintask = Task.Factory.StartNew(() => { try { proc.StandardInput.Write(stdin.ReadToEnd()); } catch { } });
+                Task stdouttask = Task.Factory.StartNew(() => { try { stdout.Write(proc.StandardOutput.ReadToEnd()); } catch { } });
+                Task stderrtask = Task.Factory.StartNew(() => { try { stderr.Write(proc.StandardError.ReadToEnd()); } catch { } });
+                proc.WaitForExit();
+                stdintask.Wait();
+                stdouttask.Wait();
+                stderrtask.Wait();
+                stdout.Write(proc.StandardOutput.ReadToEnd());
+                stderr.Write(proc.StandardError.ReadToEnd());
+                stdout.Flush();
+                stderr.Flush();
+                return proc.ExitCode;
+            }
+        }
+
+        public static int RunProcess(Stream stdin, Stream stdout, Stream stderr, string exename, string[] args)
+        {
+            return RunProcessAsUser(null, null, null, stdin, stdout, stderr, exename, args);
+        }
+
+        public static int RunProcess(TextReader stdin, TextWriter stdout, TextWriter stderr, string exename, string[] args)
+        {
+            return RunProcessAsUser(null, null, null, stdin, stdout, stderr, exename, args);
+        }
+    }
+}
--- Util/WindowsIdentityStore.cs
+++ Util/WindowsIdentityStore.cs
@@ -392,53 +392,6 @@ namespace TSVCEO.CloudPrint.Util
             cmd.ExecuteNonQuery();
         }
 
-        private static string EscapeCommandLineArgument(string arg)
-        {
-            StringBuilder sb = new StringBuilder();
-            StringReader rdr = new StringReader(arg);
-            int c;
-            sb.Append('"');
-
-            while ((c = rdr.Read()) > 0)
-            {
-                if (c == '"')
-                {
-                    sb.Append("\\\"");
-                }
-                else if (c == '\\')
-                {
-                    int nrbackslash = 1;
-
-                    while (rdr.Peek() == '\\')
-                    {
-                        nrbackslash++;
-                        rdr.Read();
-                    }
-
-                    if (rdr.Peek() == '"')
-                    {
-                        sb.Append(new String('\\', nrbackslash * 2));
-                    }
-                    else
-                    {
-                        sb.Append(new String('\\', nrbackslash));
-                    }
-                }
-                else
-                {
-                    sb.Append((char)c);
-                }
-            }
-
-            sb.Append('"');
-            return sb.ToString();
-        }
-
-        private static string CreateCommandArguments(string[] args)
-        {
-            return String.Join(" ", args.Select(s => EscapeCommandLineArgument(s)).ToArray());
-        }
-
         private static void AddUserToCurrentWindowStationDesktop(string username)
         {
             IntPtr winsta = GetProcessWindowStation();
@@ -541,60 +494,11 @@ namespace TSVCEO.CloudPrint.Util
             return GetWindowsIdentity(username).Impersonate();
         }
 
-        private static Process CreateProcessAsUser(string username, string exename, string[] args)
-        {
-            Process proc = new Process();
-
-            ProcessStartInfo startinfo = new ProcessStartInfo
-            {
-                Arguments = CreateCommandArguments(args),
-                FileName = exename,
-                CreateNoWindow = true,
-                ErrorDialog = false,
-                LoadUserProfile = false,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                StandardOutputEncoding = Encoding.UTF8,
-                StandardErrorEncoding = Encoding.UTF8,
-                UseShellExecute = false,
-                WorkingDirectory = Environment.GetEnvironmentVariable("SYSTEMDRIVE") + "\\",
-                UserName = username,
-                Domain = Domain,
-                Password = GetUserCredential(username)
-            };
-
-            proc.StartInfo = startinfo;
-
-            AddUserToCurrentWindowStationDesktop(username);
-
-            return proc;
-        }
-
         public static int RunProcessAsUser(string username, Stream stdin, Stream stdout, Stream stderr, string exename, string[] args)
         {
-            return RunProcessAsUser(username, new StreamReader(stdin, Encoding.UTF8, false), new StreamWriter(stdout, Encoding.UTF8), new StreamWriter(stderr, Encoding.UTF8), exename, args);
+            AddUserToCurrentWindowStationDesktop(username);
 
-        }
-
-        public static int RunProcessAsUser(string username, TextReader stdin, TextWriter stdout, TextWriter stderr, string exename, string[] args)
-        {
-            using (Process proc = CreateProcessAsUser(username, exename, args))
-            {
-                proc.Start();
-                Task stdintask = Task.Factory.StartNew(() => { try { proc.StandardInput.Write(stdin.ReadToEnd()); } catch { } });
-                Task stdouttask = Task.Factory.StartNew(() => { try { stdout.Write(proc.StandardOutput.ReadToEnd()); } catch { } });
-                Task stderrtask = Task.Factory.StartNew(() => { try { stderr.Write(proc.StandardError.ReadToEnd()); } catch { } });
-                proc.WaitForExit();
-                stdintask.Wait();
-                stdouttask.Wait();
-                stderrtask.Wait();
-                stdout.Write(proc.StandardOutput.ReadToEnd());
-                stderr.Write(proc.StandardError.ReadToEnd());
-                stdout.Flush();
-                stderr.Flush();
-                return proc.ExitCode;
-            }
+            return ProcessHelper.RunProcessAsUser(username, Domain, GetUserCredential(username), stdin, stdout, stderr, exename, args);
         }
 
         public static WindowsIdentity Login(string username, SecureString password)
