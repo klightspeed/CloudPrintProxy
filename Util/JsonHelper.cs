8240cea78f3580ab9952d9808fdae3c7f352b6a6
Ben Peddell <klightspeed@killerwolves.net>
2013-11-25 14:27:12 +1000

    Fix Dictionary export

--- Util/JsonHelper.cs
+++ Util/JsonHelper.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.IO;
 using System.Dynamic;
@@ -12,8 +13,10 @@ namespace TSVCEO.CloudPrint.Util
 {
     public static class JsonHelper
     {
-        private static IEnumerable<dynamic> ReadJsonArray(JsonReader reader)
+        private static dynamic ReadJsonArray(JsonReader reader)
         {
+            List<dynamic> vals = new List<dynamic>();
+
             while (reader.Read())
             {
                 if (reader.TokenType == JsonToken.EndArray)
@@ -22,9 +25,26 @@ namespace TSVCEO.CloudPrint.Util
                 }
                 else
                 {
-                    yield return ReadJsonValue(reader, true);
+                    vals.Add(ReadJsonValue(reader, true));
                 }
             }
+
+            if (vals.All(v => v is IDictionary<string, object>))
+            {
+                if (vals.OfType<IDictionary<string, object>>().All(v => v.ContainsKey("Key") && v.ContainsKey("Value")))
+                {
+                    ExpandoObject obj = new ExpandoObject();
+
+                    foreach (IDictionary<string, object> dict in vals.OfType<IDictionary<string, object>>())
+                    {
+                        ((IDictionary<string, object>)obj).Add(dict["Key"].ToString(), dict["Value"]);
+                    }
+
+                    return obj;
+                }
+            }
+
+            return vals;
         }
 
         private static ExpandoObject ReadJsonObject(JsonReader reader)
@@ -50,7 +70,7 @@ namespace TSVCEO.CloudPrint.Util
             {
                 switch (reader.TokenType)
                 {
-                    case JsonToken.StartArray: return ReadJsonArray(reader).ToList();
+                    case JsonToken.StartArray: return ReadJsonArray(reader);
                     case JsonToken.StartObject: return ReadJsonObject(reader);
                     case JsonToken.String: return reader.Value;
                     case JsonToken.Boolean: return reader.Value;
@@ -84,7 +104,7 @@ namespace TSVCEO.CloudPrint.Util
             writer.WriteEndArray();
         }
 
-        private static void WriteJsonObject(JsonWriter writer, IDictionary dict)
+        private static void WriteJsonDictionary(JsonWriter writer, IDictionary dict)
         {
             writer.WriteStartObject();
             foreach (DictionaryEntry de in dict)
@@ -95,6 +115,17 @@ namespace TSVCEO.CloudPrint.Util
             writer.WriteEndObject();
         }
 
+        private static void WriteJsonGenericDictionary(JsonWriter writer, IDictionary<string, object> dict)
+        {
+            writer.WriteStartObject();
+            foreach (KeyValuePair<string, object> de in dict)
+            {
+                writer.WritePropertyName(de.Key.ToString());
+                WriteJsonValue(writer, de.Value);
+            }
+            writer.WriteEndObject();
+        }
+        
         private static void WriteJsonObject(JsonWriter writer, object obj)
         {
             WriteJsonObject(writer, TypeDescriptor.GetProperties(obj).OfType<PropertyDescriptor>().ToDictionary(prop => prop.Name, prop => prop.GetValue(obj)));
@@ -110,7 +141,11 @@ namespace TSVCEO.CloudPrint.Util
             }
             else if (value is IDictionary)
             {
-                WriteJsonObject(writer, (IDictionary)value);
+                WriteJsonDictionary(writer, (IDictionary)value);
+            }
+            else if (value is IDictionary<string, object>)
+            {
+                WriteJsonGenericDictionary(writer, (IDictionary<string, object>)value);
             }
             else if (value is IEnumerable)
             {
